b8e6a494fc127e838de47d30998be68278cf8ad5
tabgab <gabor.tabi@gmail.com>
2023-08-17 12:48:22 +0200

    goingwild

--- audioREC.py
+++ audioREC.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from audiorecorder import audiorecorder
 import os
+import sys
 import openai
 import json
 import streamlit.components.v1 as components
@@ -82,6 +83,59 @@ def main():
 
     st.title("Talking to GPT-3")
     file_path = "audio.wav"
+
+    from contextlib import contextmanager
+from io import StringIO
+from streamlit.report_thread import REPORT_CONTEXT_ATTR_NAME
+from threading import current_thread
+
+
+@contextmanager
+def st_redirect(src, dst):
+    placeholder = st.empty()
+    output_func = getattr(placeholder, dst)
+
+    with StringIO() as buffer:
+        old_write = src.write
+
+        def new_write(b):
+            if getattr(current_thread(), REPORT_CONTEXT_ATTR_NAME, None):
+                buffer.write(b)
+                output_func(buffer.getvalue())
+            else:
+                old_write(b)
+
+        try:
+            src.write = new_write
+            yield
+        finally:
+            src.write = old_write
+
+
+@contextmanager
+def st_stdout(dst):
+    with st_redirect(sys.stdout, dst):
+        yield
+
+
+@contextmanager
+def st_stderr(dst):
+    with st_redirect(sys.stderr, dst):
+        yield
+
+
+with st_stdout("code"):
+    print("Prints as st.code()")
+
+with st_stdout("info"):
+    print("Prints as st.info()")
+
+with st_stdout("markdown"):
+    print("Prints as st.markdown()")
+
+with st_stdout("success"), st_stderr("error"):
+    print("You can print regular success messages")
+    print("And you can redirect errors as well at the same time", file=sys.stderr)
 
     logging("Starting recorder, waiting for button press.")
     if st.button("Recorder"):
